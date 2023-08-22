5cfd30264e365ba2424a3b7d769b77790e0b7743
Alejo12680 <alejo152725@gmail.com>
2023-08-22 08:27:11 -0500

    creacion de paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListTareaComponent } from './components/list-tarea/list-tarea.component';
+import { CreateTareaComponent } from './components/create-tarea/create-tarea.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:'',
+    redirectTo:'listTareas',
+    pathMatch:'full'
+  },
+  {
+    path:'listTareas',
+    component: ListTareaComponent
+  },
+  {
+    path:'createTarea',
+    component: CreateTareaComponent
+  },
+
+  // Este path siempre va a lo ultimo, es un volteo de ruta cuando el usuario escribe una URL que no corresponde a ningun componente.
+  {
+    path:'**',
+    redirectTo:'listTareas',
+    pathMatch:'full'
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
